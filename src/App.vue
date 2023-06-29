f3b38b1f026066a1b539f0434c5bf0196a3609a6
romiardisaja <romiardisaja@gmail.com>
2023-06-29 19:39:21 +0700

    add 6-lifecycle-hooks

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,28 @@ export default {
   data(){
     return{
       nama: 'Rassya',
-      products: [
-        {id: 1, title: "Product 1", price: 1000},
-        {id: 2, title: "Product 2", price: 2000},
-        {id: 3, title: "Product 3", price: 3000},
-        {id: 4, title: "Product 4", price: 4000},
-        {id: 5, title: "Product 5", price: 5000},
-      ]
+      products: []
     }
   },
+  beforeCreate(){
+    console.log('beforeCreate');
+  },
+  created(){
+    console.log('created');
+    this.products = [
+      {id: 1, title: "Product 1", price: 1000},
+      {id: 2, title: "Product 2", price: 2000},
+      {id: 3, title: "Product 3", price: 3000},
+      {id: 4, title: "Product 4", price: 4000},
+      {id: 5, title: "Product 5", price: 5000},
+    ]
+  },
+  beforeMount(){
+    console.log('beforeMount');
+  },
+  mounted(){
+    console.log('mounted');
+  }
 }
 </script>
 
