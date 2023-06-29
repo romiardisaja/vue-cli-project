ce0bb555e9e40c3bba774a08c24ca017756fdc68
romiardisaja <romiardisaja@gmail.com>
2023-06-29 19:25:36 +0700

    add 5-conditionals-and-loops

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-    <input type="text" v-model="nama">
-    {{ nama }}
+  <div v-if="products.length">
+    <ul>
+      <li v-for="item in products" :key="item.id">
+        {{ item.title }} - {{ item.price }}
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <p>No Data Found!</p>
+  </div>
 </template>
 
 <script>
@@ -9,7 +17,14 @@ export default {
   name: 'App',
   data(){
     return{
-      nama: 'Rassya'
+      nama: 'Rassya',
+      products: [
+        {id: 1, title: "Product 1", price: 1000},
+        {id: 2, title: "Product 2", price: 2000},
+        {id: 3, title: "Product 3", price: 3000},
+        {id: 4, title: "Product 4", price: 4000},
+        {id: 5, title: "Product 5", price: 5000},
+      ]
     }
   },
 }
