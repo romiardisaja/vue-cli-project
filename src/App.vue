8ce2812a48ee955b270ae2e6e6e22ede7cf27d9a
romiardisaja <romiardisaja@gmail.com>
2023-06-30 19:38:41 +0700

    add 8-components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,14 @@
 <template>
-  <input type="text" v-model="cari" placeholder="Cari Product">
-  <ul>
-    <li v-for="item in cariData" :key="item.id">
-      {{ item.title }} - {{ item.price }}
-    </li>
-  </ul>
-  
+  <Header />  
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
-  data(){
-    return{
-      cari: '',
-      products: []
-    }
-  },
-  created(){
-    console.log('created');
-    this.products = [
-      {id: 1, title: "Product 1", price: 1000},
-      {id: 2, title: "Product 2", price: 2000},
-      {id: 3, title: "Product 3", price: 3000},
-      {id: 4, title: "Product 4", price: 4000},
-      {id: 5, title: "Product 5", price: 5000},
-    ]
-  },
-  computed: {
-    cariData(){
-      return this.products.filter(item => {
-        return item.title.match(this.cari);
-      });
-    }
+  components:{
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Halaman Header</h2>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style>
+
+</style>
